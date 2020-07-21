5a118e80500931647f087db1556dbd4bda8c9d26
yshimizu-cq <y.shimizu@caraquri.com>
2020-07-21 17:47:06 +0900

    Create singup view and Add ActionBar to signup view

--- app/src/main/java/com/example/bookmanager_android/SignupActivity.java
+++ app/src/main/java/com/example/bookmanager_android/SignupActivity.java
@@ -1,8 +1,12 @@
 package com.example.bookmanager_android;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
 
 public class SignupActivity extends AppCompatActivity {
 
@@ -11,5 +15,30 @@ public class SignupActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
+        //  ツールバーの生成
+        Toolbar toolbar = findViewById(R.id.signup_tool_bar);
+        setSupportActionBar(toolbar);
+
+        //  戻るボタンの生成
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //  ヘッダ用のレイアウトファイルと紐づける
+        getMenuInflater().inflate(R.menu.menu_signup, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //  ヘッダ内のボタンを押したときの処理
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
